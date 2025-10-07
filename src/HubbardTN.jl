a71dba299ee8e1c6eb0e87e695090464ecb212e7
DaanVrancken <vrancken.daan@gmail.com>
2025-10-07 14:33:09 +0200

    remove init function

--- src/HubbardTN.jl
+++ src/HubbardTN.jl
@@ -1,12 +1,12 @@
 module HubbardFunctions
 
-if occursin("amd",lowercase(Sys.cpu_info()[1].model))
-    using BLISBLAS
-    println("Using BLIS for BLAS operations.")
-else
-    using MKL
-    println("Using MKL for BLAS and LAPACK operations.")
-end
+# if occursin("amd",lowercase(Sys.cpu_info()[1].model))
+#     using BLISBLAS
+#     println("Using BLIS for BLAS operations.")
+# else
+#     using MKL
+#     println("Using MKL for BLAS and LAPACK operations.")
+# end
 
 export OB_Sim, MB_Sim, OBC_Sim, MBC_Sim
 export produce_groundstate, produce_excitations, produce_bandgap, produce_TruncState
@@ -25,18 +25,18 @@ using Plots.PlotMeasures
 using TensorOperations
 using JLD2
 
-function __init__()
-    LinearAlgebra.BLAS.set_num_threads(1)
-    if haskey(ENV, "SLURM_JOB_ID") || haskey(ENV, "JOB_ID") || haskey(ENV, "PBS_JOBID")
-        # Running on remote cluster
-        ThreadPinning.pinthreads(:affinitymask)
-    else
-        # Running locally
-        ThreadPinning.pinthreads(:cores)
-    end
-    MPSKit.Defaults.set_scheduler!(:dynamic)   # serial -> disable multithreading, greedy -> greedy load-balancing, dynamic -> moderate load-balancing
-    println("Running on $(Threads.nthreads()) threads.")
-end
+# function __init__()
+#     LinearAlgebra.BLAS.set_num_threads(1)
+#     if haskey(ENV, "SLURM_JOB_ID") || haskey(ENV, "JOB_ID") || haskey(ENV, "PBS_JOBID")
+#         # Running on remote cluster
+#         ThreadPinning.pinthreads(:affinitymask)
+#     else
+#         # Running locally
+#         ThreadPinning.pinthreads(:cores)
+#     end
+#     MPSKit.Defaults.set_scheduler!(:dynamic)   # serial -> disable multithreading, greedy -> greedy load-balancing, dynamic -> moderate load-balancing
+#     println("Running on $(Threads.nthreads()) threads.")
+# end
 
 include("simulations.jl")
 include("hamiltonian.jl")
