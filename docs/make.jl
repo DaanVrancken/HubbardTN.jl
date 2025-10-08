2f4081032dcd8e08ffc4383c07f4aac37e47337c
DaanVrancken <vrancken.daan@gmail.com>
2025-10-08 16:23:31 +0200

    set up CI workflow

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,28 @@
 using Documenter
 using FilePathsBase
+using HubbardTN
 
 # For local usage: dirty workaround for limited acces rights of Julia.
 # Might give troubles when ran on other system -> remove following 2 lines.
 # If necessary, delete old "build" file manaually
-build_path = joinpath(pwd(),"build")
-if isdir(build_path)
-    run(`powershell -Command "Remove-Item -Recurse -Force '$build_path'"`)
-end
-
-include(joinpath("..","src","HubbardFunctions.jl"))
+# build_path = joinpath(pwd(),"build")
+# if isdir(build_path)
+#     run(`powershell -Command "Remove-Item -Recurse -Force '$build_path'"`)
+# end
 
 makedocs(
-    sitename = "HubbardTN",
-    modules = [HubbardFunctions],
+    sitename = "HubbardTN.jl",
+    modules = [HubbardTN],
     pages = [
         "Home" => "index.md",
         "Library" => "Functions.md",
         "Examples" => "Examples.md",
     ],
     format = Documenter.HTML(inventory_version = "0.1.0"),
-    clean = true,
+)
+
+deploydocs(
+    repo = "github.com/dvrancke/HubbardTN.jl.git",
+    push_preview = true,
+    devbranch = "master",
 )
